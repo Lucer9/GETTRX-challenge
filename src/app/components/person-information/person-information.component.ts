e76cf4d94400a99da2e460004396fbdaa5ca34f6
Carlos Aguirre <carlos.aguirreorozco@gmail.com>
2023-02-14 23:41:15 -0600

    removing unused interface

--- src/app/components/person-information/person-information.component.ts
+++ src/app/components/person-information/person-information.component.ts
@@ -2,15 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { PeopleService } from 'src/app/services/people.service';
 
-interface Person {
-  name: string;
-  height: string;
-  mass: string;
-  skin_color: string;
-  birth_year: string;
-  hair_color: string;
-  eye_color: string;
-}
 @Component({
   selector: 'app-person-information',
   templateUrl: './person-information.component.html',
